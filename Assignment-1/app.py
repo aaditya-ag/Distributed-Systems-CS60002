4788f365aabcbff69f6cbe2f72863391c8223544
Riope <59020035+Riope@users.noreply.github.com>
2023-01-26 13:19:06 +0530

    Added APIs
    Added Enqueue, Dequeue, and Size

--- Assignment-1/app.py
+++ Assignment-1/app.py
@@ -12,6 +12,20 @@ topics = [
     "bye"
 ]
 
+producers = {
+    "hello": [0,1],
+    "bye": [0]
+}
+
+consumers = {
+    "hello": [[0,0]],
+    "bye": [[0,0], [1,0]]
+}
+
+logs = {
+    "hello": ["msg1", "msg2"],
+    "bye": ["msg1"]
+}
 
 class Topics(Resource):
     parser = reqparse.RequestParser()
@@ -62,9 +76,99 @@ class ProducerRegister(Resource):
             "message": "Topic '" + request.get_json()["topic"] + "' created."
         }
 
+class Enqueue(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('topic', required = True, help = 'topic name for the message to be added')
+    parser.add_argument('producer_id', required = True, help = 'producer ID of the client')
+    parser.add_argument('message', required = True, help = 'message to be added to the queue')
+
+    def post(self):
+        args = Enqueue.parser.parse_args()
+        
+        if args["topic"] not in topics:
+            return {
+                "status": "Failure",
+                "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
+            }
+        if int(args["producer_id"]) not in producers[args["topic"]]:
+            return {
+                "status": "Failure",
+                "message": "Producer ID '" + request.get_json()["producer_id"] + "' is not registered for the given topic."
+            }
+        logs[args["topic"]].append(args["message"])
+        return {
+            "status": "Success",
+            "message": "Message '" + request.get_json()["message"] + "' added for the topic."
+        }
+
+class Dequeue(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('topic', required = True, help = 'topic name for the message to be added')
+    parser.add_argument('consumer_id', required = True, help = 'consumer ID of the client')
+
+    def get(self):
+        args = Dequeue.parser.parse_args()
+        
+        if args["topic"] not in topics:
+            return {
+                "status": "Failure",
+                "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
+            }
+        cons_id = [i[0] for i in consumers[args["topic"]]]
+        if int(args["consumer_id"]) not in cons_id:
+            return {
+                "status": "Failure",
+                "message": "Consumer ID '" + request.get_json()["consumer_id"] + "' is not registered for the given topic."
+            }
+        ind = consumers[args["topic"]][cons_id.index(int(args["consumer_id"]))][1]
+        if ind >= len(logs[args["topic"]]):
+             return {
+                "status": "Failure",
+                "message": "No new updates/messages for the given topic."
+            }
+        msg = logs[args["topic"]][ind]
+        return {
+            "status": "Success",
+            "message": "Message " + msg + " retrieved for the topic."
+        }
+    
+class Size(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('topic', required = True, help = 'topic name for the message to be added')
+    parser.add_argument('consumer_id', required = True, help = 'consumer ID of the client')
+
+    def get(self):
+        args = Size.parser.parse_args()
+        
+        if args["topic"] not in topics:
+            return {
+                "status": "Failure",
+                "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
+            }
+        cons_id = [i[0] for i in consumers[args["topic"]]]
+        if int(args["consumer_id"]) not in cons_id:
+            return {
+                "status": "Failure",
+                "message": "Consumer ID '" + request.get_json()["consumer_id"] + "' is not registered for the given topic."
+            }
+        ind = consumers[args["topic"]][cons_id.index(int(args["consumer_id"]))][1]
+        if ind >= len(logs[args["topic"]]):
+             return {
+                "status": "Success",
+                "message": "No new updates/messages for the given topic."
+            }
+        size = len(logs[args["topic"]]) - ind
+        return {
+            "status": "Success",
+            "message": str(size) + " updates/messages for the given topic."
+        }
+
 api.add_resource(Topics, '/topics')
 api.add_resource(ConsumerRegister, '/consumer/register')
+api.add_resource(Dequeue, '/consumer/consume')
 api.add_resource(ProducerRegister, '/producer/register')
+api.add_resource(Enqueue, '/producer/produce')
+api.add_resource(Size, '/size')
 
 
 if __name__ == '__main__':
