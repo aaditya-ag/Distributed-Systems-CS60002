84ea261b0d5bf9597ec39d8f88dd455661af6979
Pritkumar Godhani <pritgodhani132@gmail.com>
2023-01-30 23:48:42 +0530

    Completed locking mechanism for PartA

--- Assignment-1/app.py
+++ Assignment-1/app.py
@@ -1,38 +1,98 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
+import threading
+import time
 
 app = Flask(__name__)
 api = Api(app)
 
+# topics = [
+#     "hello",
+#     "bye"
+# ]
 
-# This is a basic app.py, doesn't have logic implemented in it
+# producers = {
+#     "hello": [0,1],
+#     "bye": [0]
+# }
+
+# consumers = {
+#     "hello": [[0,0]],
+#     "bye": [[0,0], [1,0]]
+# }
+
+# logs = {
+#     "hello": ["msg1", "msg2"],
+#     "bye": ["msg1"]
+# }
 
 
-# Dictionary: indexed by topic name
-# Each entry is a list of tuples (a,b) 
-# where a = topic-specific consumer id
-# and b = next read index for the consumer 
+class Producers:
+    def __init__(self):
+        self.producers = {}
+        self.lock = threading.Semaphore()
+        self.count = 0
 
-topics = [
-    "hello",
-    "bye"
-]
+    def get_topics(self):
+        return self.producers.keys()
 
-producers = {
-    "hello": [0,1],
-    "bye": [0]
-}
+    def add_producer(self, topic):
+        next_producer_id = 0
+        self.lock.acquire()
+        if(topic not in self.get_topics()):
+            self.producers[topic] = []
+        next_producer_id = self.count
+        self.producers[topic].append(next_producer_id)
+        self.count += 1
+        self.lock.release()
+        return next_producer_id
 
-consumers = {
-    "hello": [[0,0]],
-    "bye": [[0,0], [1,0]]
-}
 
-logs = {
-    "hello": ["msg1", "msg2"],
-    "bye": ["msg1"]
-}
+class Consumers: 
+    def __init__(self):
+        self.consumers = {}
+        self.lock = threading.Semaphore()
+        self.count = 0
+    
+    def get_topics(self):
+        return self.consumers.keys()
 
+    def add_consumer(self, topic):
+        next_consumer_id = 0
+        self.lock.acquire()
+        if(topic not in self.get_topics()):
+            self.consumers[topic] = []
+        next_consumer_id = self.count
+        self.consumers[topic].append([next_consumer_id,0])
+        self.count += 1
+        self.lock.release()
+        return next_consumer_id
+
+    
+class MasterQueue:
+    def __init__(self):
+        self.queues = {} # basically, the logs dictionary
+        self.locks = {}
+        self.global_lock = threading.Semaphore()
+    
+    def get_topics(self):
+        return list(self.queues.keys())
+
+    def add_topic(self, topic):
+        self.global_lock.acquire()
+        self.queues[topic] = []
+        self.locks[topic] = threading.Semaphore()
+        self.global_lock.release()
+
+    def add_message(self, topic, message):
+        self.locks[topic].acquire()
+        self.queues[topic].append(message)
+        self.locks[topic].release()
+    
+
+m_queue = MasterQueue()
+m_producers = Producers()
+m_consumers = Consumers()
 
 class Topics(Resource):
     parser = reqparse.RequestParser()
@@ -41,13 +101,13 @@ class Topics(Resource):
     def get(self):
         return {
             "status": "Success",
-            "topics": topics
+            "topics": m_queue.get_topics()
         }
 
     def post(self):
         args = Topics.parser.parse_args()
-        if(args["name"] not in topics): 
-            topics.append(args["name"])
+        if(args["name"] not in m_queue.get_topics()): 
+            m_queue.add_topic(args["name"])
             return {
                 "status": "Success",
                 "message": "Topic \'" + request.get_json()["name"] + "\' created."
@@ -68,26 +128,29 @@ class ConsumerRegister(Resource):
         ## DEBUG ##
         print(args["topic"])
         ###########
-
-        if args["topic"] not in topics:
+        topics = m_queue.get_topics()
+        if args["topic"] not in topics :
             return {
                 "status": "Failure",
                 "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
             }
         else:
-            next_consumer_id = 0
-
-            if(args["topic"] not in consumers.keys()):
-                consumers[args["topic"]] = []
-                consumers[args["topic"]].append([next_consumer_id,0])
-            else: 
-                for _consumer in consumers[args["topic"]]:
-                    next_consumer_id = max(next_consumer_id, _consumer[0])
-                next_consumer_id += 1
-                consumers[args["topic"]].append([next_consumer_id,0])
+            next_consumer_id = m_consumers.add_consumer(args["topic"])
+            # if(args["topic"] not in m_consumers.get_topics()):
+            #     m_consumers.lock.acquire()
+            #     m_consumers.consumers[args["topic"]] = []
+            #     m_consumers.consumers[args["topic"]].append([next_consumer_id,0])
+            #     m_consumers.lock.release()
+            # else: 
+            #     for _consumer in m_consumers.get_topics():
+            #         next_consumer_id = max(next_consumer_id, _consumer[0])
+            #     next_consumer_id += 1
+            #     m_consumers.lock.acquire()
+            #     m_consumers.consumers[args["topic"]].append([next_consumer_id,0])
+            #     m_consumers.lock.release()
 
             ## DEBUG ##
-            print(consumers)
+            print(m_consumers.consumers)
             ###########
 
             return {
@@ -107,25 +170,29 @@ class ProducerRegister(Resource):
         print(args["topic"])
         ###########
 
-        next_producer_id = 0
-        if args["topic"] not in topics:
-            # if topic doesnt exist, add the topic
-            topics.append(args["topic"])
-            # since topic is newly added, create a new topic entry in the producers dict
-            producers[args["topic"]] = [next_producer_id]
-        else:
-            # topic exist but no one has registered to it as producer
-            if(args["topic"] not in producers.keys()):
-                producers[args["topic"]] = [next_producer_id]
-            # already existing entries for producers of the topic
-            else:
-                for _producer in producers[args["topic"]]:
-                    next_producer_id = max(_producer, next_producer_id)
-                next_producer_id += 1
-                producers[args["topic"]].append(next_producer_id)
+        if(args["topic"] not in m_queue.get_topics()):
+            m_queue.add_topic(args["topic"])
+
+        next_producer_id = m_producers.add_producer(args["topic"])
+        # next_producer_id = 0
+        # if args["topic"] not in topics:
+        #     # if topic doesnt exist, add the topic
+        #     topics.append(args["topic"])
+        #     # since topic is newly added, create a new topic entry in the producers dict
+        #     producers[args["topic"]] = [next_producer_id]
+        # else:
+        #     # topic exist but no one has registered to it as producer
+        #     if(args["topic"] not in producers.keys()):
+        #         producers[args["topic"]] = [next_producer_id]
+        #     # already existing entries for producers of the topic
+        #     else:
+        #         for _producer in producers[args["topic"]]:
+        #             next_producer_id = max(_producer, next_producer_id)
+        #         next_producer_id += 1
+        #         producers[args["topic"]].append(next_producer_id)
 
         ## DEBUG ##
-        print(producers)
+        print(m_producers.producers)
         ###########
 
         return {
@@ -143,17 +210,18 @@ class Enqueue(Resource):
     def post(self):
         args = Enqueue.parser.parse_args()
         
-        if args["topic"] not in topics:
+        if args["topic"] not in m_queue.get_topics():
             return {
                 "status": "Failure",
                 "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
             }
-        if int(args["producer_id"]) not in producers[args["topic"]]:
+        if int(args["producer_id"]) not in m_producers.producers[args["topic"]]:
             return {
                 "status": "Failure",
                 "message": "Producer ID '" + request.get_json()["producer_id"] + "' is not registered for the given topic."
             }
-        logs[args["topic"]].append(args["message"])
+        m_queue.add_message(args["topic"], args["message"])
+        # logs[args["topic"]].append(args["message"])
         return {
             "status": "Success",
             "message": "Message '" + request.get_json()["message"] + "' added for the topic."
@@ -167,24 +235,24 @@ class Dequeue(Resource):
     def get(self):
         args = Dequeue.parser.parse_args()
         
-        if args["topic"] not in topics:
+        if args["topic"] not in m_queue.get_topics():
             return {
                 "status": "Failure",
                 "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
             }
-        cons_id = [i[0] for i in consumers[args["topic"]]]
+        cons_id = [i[0] for i in m_consumers.consumers[args["topic"]]]
         if int(args["consumer_id"]) not in cons_id:
             return {
                 "status": "Failure",
                 "message": "Consumer ID '" + request.get_json()["consumer_id"] + "' is not registered for the given topic."
             }
-        ind = consumers[args["topic"]][cons_id.index(int(args["consumer_id"]))][1]
-        if ind >= len(logs[args["topic"]]):
+        ind = m_consumers.consumers[args["topic"]][cons_id.index(int(args["consumer_id"]))][1]
+        if ind >= len( m_queue.queues[args["topic"]] ):
              return {
                 "status": "Failure",
                 "message": "No new updates/messages for the given topic."
             }
-        msg = logs[args["topic"]][ind]
+        msg = m_queue.queues[args["topic"]][ind]
         return {
             "status": "Success",
             "message": "Message " + msg + " retrieved for the topic."
@@ -198,24 +266,24 @@ class Size(Resource):
     def get(self):
         args = Size.parser.parse_args()
         
-        if args["topic"] not in topics:
+        if args["topic"] not in m_queue.get_topics():
             return {
                 "status": "Failure",
                 "message": "Topic '" + request.get_json()["topic"] + "' doesn't exist."
             }
-        cons_id = [i[0] for i in consumers[args["topic"]]]
+        cons_id = [i[0] for i in m_consumers.consumers[args["topic"]]]
         if int(args["consumer_id"]) not in cons_id:
             return {
                 "status": "Failure",
                 "message": "Consumer ID '" + request.get_json()["consumer_id"] + "' is not registered for the given topic."
             }
-        ind = consumers[args["topic"]][cons_id.index(int(args["consumer_id"]))][1]
-        if ind >= len(logs[args["topic"]]):
+        ind = m_consumers.consumers[args["topic"]][cons_id.index(int(args["consumer_id"]))][1]
+        if ind >= len(m_queue.queues[args["topic"]]):
              return {
                 "status": "Success",
                 "message": "No new updates/messages for the given topic."
             }
-        size = len(logs[args["topic"]]) - ind
+        size = len(m_queue.queues[args["topic"]]) - ind
         return {
             "status": "Success",
             "message": str(size) + " updates/messages for the given topic."
