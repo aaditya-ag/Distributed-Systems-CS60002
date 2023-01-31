59cc5c6d801c3a85113c072cf2d5fcee23060ee9
Aaditya Agrawal <aaditya1106@gmail.com>
2023-01-31 23:24:02 +0530

    Setup Library

---
+++ Assignment-1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='myqueue_library',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Distributed Logging Queue',
+    author='Aaditya Agrawal',
+    license='MIT',
+)
